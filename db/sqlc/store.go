751f49f08bbda54b6d207dad836e05c17e4b09dc
Luis Antunes <luisantunes0914@gmail.com>
2024-01-18 23:43:01 -0300

    adicionando o store ao db e os testes unitários

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 // Store provides all functions to execute db queries and transactins
@@ -21,30 +22,73 @@ func NewStore(db *sql.DB) *Store {
 
 // execTx executes a function within a database transactins
 func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
-    tx, err := store.db.BeginTx(ctx, nil)
-    if err != nil {
-        return eri
-    }
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
 
-    q := New(tx)
-    err = fn(q)
-    if err != nil {
-        rbErr := tx.Rollback(); rbErr != nil {
-            return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
-        }
-        return err
-    }
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
 
-    return tx.Commit()
+	return tx.Commit()
 }
 
-// TransferTxParams contains the input parameters of the transfer transactins
+// TransferTxParams contains the input parameters of the transfer transaction
 type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
 
+// TranderTxResult is the result of the transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
 }
 
 // TransferTx performs a money transfer from one account to the other.
-//T It creates a transfer record, and account entries, and update accounts balance within a single database transactins
+// It creates a transfer record, and account entries, and update accounts balance within a single database transactins
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
 
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	return result, err
 }
