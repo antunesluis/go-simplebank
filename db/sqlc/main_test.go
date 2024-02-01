562ebdd5bf783f83d9894bc7f91729c0b6614535
Luis Antunes <luisantunes0914@gmail.com>
2024-02-01 01:36:34 -0300

    aadicionando restante implementação da rest api e inicio do mockDB

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,12 +6,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/antunesluis/simplebank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var (
@@ -20,9 +16,12 @@ var (
 )
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot open config:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource) // Use the package-level testDB variable
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 
 	"github.com/antunesluis/simplebank/api"
 	db "github.com/antunesluis/simplebank/db/sqlc"
+	"github.com/antunesluis/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver     = "postgres"
-	dbSource     = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAdress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource) // Use the package-level testDB variable
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource) // Use the package-level testDB variable
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAdress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server")
 	}
