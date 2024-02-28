2e8a686db2de89d3f97946309a4536f083fcdef2
Luis Antunes <luisantunes0914@gmail.com>
2024-02-27 23:47:41 -0300

    add transfer.go file

---
+++ api/trasfer.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/antunesluis/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type transferRequest struct {
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountId   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,oneof=USD EUR CAD"`
+}
+
+func (server *Server) createTransfer(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
